f1a5396cc18cfdf76ae5b64372360bfe3eecacad
KhushPatibandha <khush.patibandha@gmail.com>
2023-08-26 22:14:01 +0530

    --

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ import multer from "multer"
 import helmet from "helmet"
 import morgan from "morgan"
 import path from "path"
+import ocrSpaceApi from 'ocr-space-api-wrapper';
 import { fileURLToPath } from "url"
 import authRoutes from "./routes/auth.js"
 import patientRouters from "./routes/patient.js"
@@ -38,7 +39,6 @@ const upload = multer({ storage })
 
 // ROUTES WITH FILES
 // FOR OCR
-
 
 
 // ROUTES
