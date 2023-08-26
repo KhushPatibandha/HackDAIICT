e887324ad660735e87c15e68282706932c904dcf
KhushPatibandha <khush.patibandha@gmail.com>
2023-08-26 23:14:28 +0530

    height, weight, test added with routes, controllers

--- server/routes/patient.js
+++ server/routes/patient.js
@@ -1,12 +1,25 @@
 import express from "express"
 import { verifyToken } from "../middleware/auth.js"
-import { getPatient, postPatient, getPatientHistory, updatePatient } from "../controllers/patient.js"
+import { 
+    getPatient, 
+    postPatient, 
+    getPatientPrescriptionsHistory, 
+    updatePatient, 
+    getPatientPrescriptions, 
+    getPreviousIllness,
+    getPreviousTest,
+    getUpComingTest
+} from "../controllers/patient.js"
 
 const router = express.Router()
 
 router.post("/",  verifyToken, postPatient)
 router.get("/:abdmNumber", verifyToken, getPatient)
 router.put("/:abdmNumber", verifyToken, updatePatient)
-router.get("/:abdmNumber/history", verifyToken, getPatientHistory)
+router.get("/:abdmNumber/prescriptionHistory", verifyToken, getPatientPrescriptionsHistory)
+router.get("/:abdmNumber/prescriptions", verifyToken, getPatientPrescriptions)
+router.get("/:abdmNumber/previousIllness", verifyToken, getPreviousIllness)
+router.get("/:abdmNumber/previousTest", verifyToken, getPreviousTest)
+router.get("/:abdmNumber/upComingTest", verifyToken, getUpComingTest)
 
 export default router
